acc530e35113430e13b4a4e2361d3f1952439634
jsergent <julien.a.sergent@capgemini.com>
2017-08-23 14:59:39 +0200

    fix(npm): start script

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const database = firebase.database
 const snippets = database.ref('/snippets')
 
 // listen for changes to firebase data
-snippets.on('child_added', snapshot => elastic.create('firebase', 'snippet', snapshot));
-snippets.on('child_changed', snapshot => elastic.update('firebase', 'snippet', snapshot));
-snippets.on('child_removed', snapshot => elastic.delete('firebase', 'snippet', snapshot));
+snippets.on('child_added', snapshot => elastic.create('firebase', 'snippet', snapshot))
+snippets.on('child_changed', snapshot => elastic.update('firebase', 'snippet', snapshot))
+snippets.on('child_removed', snapshot => elastic.delete('firebase', 'snippet', snapshot))
