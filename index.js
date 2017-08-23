d9e4adca1eb026b46ba2a49ad88938e5716f4301
jsergent <julien.a.sergent@capgemini.com>
2017-08-23 16:48:10 +0200

    refactor(Procfile): change web to worker task

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ const snippets = database.ref('/snippets')
 snippets.on('child_added', snapshot => elastic.create('firebase', 'snippet', snapshot))
 snippets.on('child_changed', snapshot => elastic.update('firebase', 'snippet', snapshot))
 snippets.on('child_removed', snapshot => elastic.delete('firebase', 'snippet', snapshot))
+
+console.log('Firebase listeners started.')
