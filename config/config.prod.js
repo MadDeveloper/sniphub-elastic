8bae502ec7c2a744e7f87de75668e3050213fc46
MadDeveloper <sergent.julien@icloud.com>
2017-11-21 12:19:00 +0100

    Add prod config

---
+++ config/config.prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+    elastic: {
+        url: 'https://5s886dfw:z7gjc4csm6ylm3zo@pine-2241382.us-east-1.bonsaisearch.net'
+    },
+    firebase: {
+        apiKey: "AIzaSyD_WQDAkekvaAB2c0m3YNz9eGoDSSs1DOQ",
+        authDomain: "sniphub-prod.firebaseapp.com",
+        databaseURL: "https://sniphub-prod.firebaseio.com",
+        projectId: "sniphub-prod",
+        storageBucket: "sniphub-prod.appspot.com",
+        messagingSenderId: "628588964087"
+    }
+}
